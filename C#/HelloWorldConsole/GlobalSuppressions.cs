ddd8b43cdecc1bb63602fa3bf8fdba9460bae077
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-11-02 11:22:56 -0700

    Having the necessary hello world console application

---
+++ C#/HelloWorldConsole/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Blaze Spaces">
+// Copyright (c) Blaze Spaces. All rights reserved.
+// </copyright>
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "Matches", Scope = "namespace", Target = "~N:HelloWorldConsole")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "Matches")]
---
+++ C#/HelloWorldConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Blaze Spaces">
+// Copyright (c) Blaze Spaces. All rights reserved.
+// </copyright>
+
+namespace HelloWorldConsole
+{
+    using System;
+
+    /// <summary>
+    /// This is the main driver class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// This is the main driver method.
+        /// </summary>
+        /// <param name="args">Project specific arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
